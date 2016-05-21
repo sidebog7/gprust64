8e3148c974dc7983c519d14aa25e309ef0e59cd9
Gareth Pendleton <gareth.pendleton@ninthwave.co.uk>
2016-05-21 21:58:18 +0100

    use Default trait

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,7 @@ use std::path::Path;
 const NUM_GPREG: usize = 32;
 const NUM_FPREG: usize = 32;
 
+#[derive(Default)]
 struct Cpu {
     reg_gprs: [u64; NUM_GPREG],
     reg_fprs: [u64; NUM_FPREG],
@@ -25,22 +26,7 @@ struct Cpu {
 
 impl Cpu {
     fn new() -> Cpu {
-        Cpu {
-            reg_gprs: [0; NUM_GPREG],
-            reg_fprs: [0; NUM_FPREG],
-
-            reg_pc: 0,
-
-            reg_hi: 0,
-            reg_lo: 0,
-
-            reg_llbit: false,
-
-            reg_fcr0: 0,
-            reg_fcr31: 0,
-
-            cp0: CP0::new()
-        }
+        Cpu::default()
     }
 
     fn power_on_reset(&mut self) {
@@ -48,6 +34,7 @@ impl Cpu {
     }
 }
 
+#[derive(Default)]
 struct CP0 {
     reg_index: u64,
     reg_random: u64,
@@ -76,35 +63,11 @@ struct CP0 {
 
 impl CP0 {
     fn new() -> CP0 {
-        CP0 {
-            reg_index: 0,
-            reg_random: 0,
-            reg_entrylo0: 0,
-            reg_entrylo1: 0,
-            reg_context: 0,
-            reg_pagemask: 0,
-            reg_wired: 0,
-            reg_badvaddr: 0,
-            reg_count: 0,
-            reg_entryhi: 0,
-            reg_compare: 0,
-            reg_status: 0,
-            reg_cause: 0,
-            reg_epc: 0,
-            reg_prid: 0,
-            reg_config: 0,
-            reg_lladdr: 0,
-            reg_watchlo: 0,
-            reg_watchhi: 0,
-            reg_xcontext: 0,
-            reg_taglo: 0,
-            reg_taghi: 0,
-            reg_errorepc: 0
-        }
+        CP0::default()
     }
 
     fn power_on_reset(&mut self) {
-        
+
     }
 }
 
