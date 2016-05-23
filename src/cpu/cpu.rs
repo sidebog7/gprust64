8cbbe85e65b303f153502f94cd86089141b5bcf5
Gareth Pendleton <gareth.pendleton@ninthwave.co.uk>
2016-05-23 22:22:51 +0100

    Added debug trait to Cpu

--- src/cpu/cpu.rs
+++ src/cpu/cpu.rs
@@ -2,12 +2,13 @@ use super::super::bus;
 use super::cp0::CP0;
 use super::instruction::Instruction;
 
+use std::fmt;
+
 const NUM_GPREG: usize = 32;
 const NUM_FPREG: usize = 32;
 
 const PIF_ROM_START: u64 = 0xffff_ffff_bfc0_0000;
 
-#[derive(Debug)]
 pub struct Cpu {
     reg_gprs: [u64; NUM_GPREG],
     reg_fprs: [f64; NUM_FPREG],
@@ -25,6 +26,54 @@ pub struct Cpu {
     cp0: CP0,
 
     bus: bus::Bus,
+}
+
+impl fmt::Debug for Cpu {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        const REGS_PER_LINE: usize = 2;
+        const REG_NAMES: [&'static str; NUM_GPREG] =
+            ["r0", "at", "v0", "v1", "a0", "a1", "a2", "a3", "t0", "t1", "t2", "t3", "t4", "t5",
+             "t6", "t7", "s0", "s1", "s2", "s3", "s4", "s5", "s6", "s7", "t8", "t9", "k0", "k1",
+             "gp", "sp", "s8", "ra"];
+
+        try!(write!(f, "\nCPU General Purpose Registers:"));
+        for reg_num in 0..NUM_GPREG {
+            if (reg_num % REGS_PER_LINE) == 0 {
+                try!(writeln!(f, ""));
+            }
+            try!(write!(f,
+                        "{reg_name}/gpr{num:02}: {value:#018X} ",
+                        num = reg_num,
+                        reg_name = REG_NAMES[reg_num],
+                        value = self.reg_gprs[reg_num]));
+        }
+
+        try!(write!(f, "\n\nCPU Floating Point Registers:"));
+        for reg_num in 0..NUM_FPREG {
+            if (reg_num % REGS_PER_LINE) == 0 {
+                try!(writeln!(f, ""));
+            }
+            try!(write!(f,
+                "fpr{num:02}: {value:21} ",
+                num = reg_num,
+                value = self.reg_fprs[reg_num],));
+        }
+
+        try!(writeln!(f, "\n\nCPU Special Registers:"));
+        try!(writeln!(f,
+                      "\
+            reg_pc: {:#018X}\nreg_hi: {:#018X}\nreg_lo: \
+                       {:#018X}\nreg_llbit: {}\nreg_fcr0:  {:#018X}\nreg_fcr31: {:#018X}\n",
+                      self.reg_pc,
+                      self.reg_hi,
+                      self.reg_lo,
+                      self.reg_llbit,
+                      self.reg_fcr0,
+                      self.reg_fcr31));
+
+        try!(writeln!(f, "{:#?}", self.cp0));
+        writeln!(f, "{:#?}", self.bus)
+    }
 }
 
 impl Cpu {
