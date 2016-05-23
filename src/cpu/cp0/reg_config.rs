8d466c59dfb57382d42ea6d57ec7a718e2446f60
Gareth Pendleton <gareth.pendleton@ninthwave.co.uk>
2016-05-23 13:02:33 +0100

    renamed config register properies

--- src/cpu/cp0/reg_config.rs
+++ src/cpu/cp0/reg_config.rs
@@ -1,93 +1,93 @@
 
 #[derive(Default, Debug)]
 pub struct RegConfig {
-    ep: EP,
-    be: BE,
-    k0: K0,
+    transfer_data_pattern: TransferDataPattern,
+    endianness: Endianness,
+    coherency_algorithm: CoherencyAlgorithm,
 }
 
 impl RegConfig {
     pub fn power_on_reset(&mut self) {
-        self.ep = EP::D;
-        self.be = BE::BigEndian;
+        self.transfer_data_pattern = TransferDataPattern::D;
+        self.endianness = Endianness::BigEndian;
     }
 }
 
 impl From<u32> for RegConfig {
     fn from(data: u32) -> Self {
         RegConfig {
-            ep: data.into(),
-            be: data.into(),
-            k0: data.into(),
+            transfer_data_pattern: data.into(),
+            endianness: data.into(),
+            coherency_algorithm: data.into(),
         }
     }
 }
 
 #[derive(Debug)]
-enum EP {
+enum TransferDataPattern {
     D,
     DxxDxx,
     RFU,
 }
 
-impl Default for EP {
+impl Default for TransferDataPattern {
     fn default() -> Self {
-        EP::D
+        TransferDataPattern::D
     }
 }
 
-impl From<u32> for EP {
+impl From<u32> for TransferDataPattern {
     fn from(f: u32) -> Self {
-        let epdata = (f >> 24) & 0b1111;
-        match epdata {
-            0 => EP::D,
-            6 => EP::DxxDxx,
-            _ => EP::RFU,
+        let transfer_data_patterndata = (f >> 24) & 0b1111;
+        match transfer_data_patterndata {
+            0 => TransferDataPattern::D,
+            6 => TransferDataPattern::DxxDxx,
+            _ => TransferDataPattern::RFU,
         }
     }
 }
 
 #[derive(Debug)]
-enum BE {
+enum Endianness {
     LittleEndian,
     BigEndian,
 }
 
-impl Default for BE {
+impl Default for Endianness {
     fn default() -> Self {
-        BE::BigEndian
+        Endianness::BigEndian
     }
 }
 
-impl From<u32> for BE {
+impl From<u32> for Endianness {
     fn from(f: u32) -> Self {
-        let bedata = (f >> 15) & 0b1;
-        if bedata == 0 {
-            BE::LittleEndian
+        let endiannessdata = (f >> 15) & 0b1;
+        if endiannessdata == 0 {
+            Endianness::LittleEndian
         } else {
-            BE::BigEndian
+            Endianness::BigEndian
         }
     }
 }
 
 #[derive(Debug)]
-enum K0 {
+enum CoherencyAlgorithm {
     NotUsed,
     Used,
 }
 
-impl Default for K0 {
+impl Default for CoherencyAlgorithm {
     fn default() -> Self {
-        K0::NotUsed
+        CoherencyAlgorithm::NotUsed
     }
 }
 
-impl From<u32> for K0 {
+impl From<u32> for CoherencyAlgorithm {
     fn from(f: u32) -> Self {
-        let k0data = f & 0b111;
-        match k0data {
-            0b010 => K0::NotUsed,
-            _ => K0::Used,
+        let coherency_algorithmdata = f & 0b111;
+        match coherency_algorithmdata {
+            0b010 => CoherencyAlgorithm::NotUsed,
+            _ => CoherencyAlgorithm::Used,
         }
     }
 }
